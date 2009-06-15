8adbe1fb403c162f33bb9331a03a2876a57df958
spox <spox@modspox.com>
2009-06-15 16:40:39 -0700

    add code formatting

--- helper/codebatter.rb
+++ helper/codebatter.rb
@@ -1,17 +1,21 @@
+require 'uv'
 require 'htmlentities'
 module Ramaze
     module Helper
         module Codebatter
+            SYN_LANGS=Uv.syntaxes.map{|x| x.downcase}
             def code_insert(id)
                 coder = HTMLEntities.new
                 code = Models::Code[id]
                 output = nil
                 if(code)
-                    output = coder.encode(code.code)
+                    lang = nil
+                    code.tags.each{|t| lang = t.name if SYN_LANGS.include?(t.name.downcase)}
+                    output = lang.nil? ? "<pre>#{coder.encode(code.code)}</pre>" : Uv.parse(code.code, 'xhtml', 'ruby', true, 'lazy')
                 else
-                    output = 'Error: Failed to find requested code item'
+                    output = '<pre>Error: Failed to find requested code item</pre>'
                 end
-                return "<pre>#{output}</pre>"
+                return output
             end
         end
     end
