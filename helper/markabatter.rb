0e4c3984ec35d290ddc6397a963b0081a5af98fb
spox <spox@modspox.com>
2009-06-26 08:35:56 -0700

    better cutting

--- helper/markabatter.rb
+++ helper/markabatter.rb
@@ -12,10 +12,15 @@ module Ramaze
             # This will take a string of Markaby and return the
             # results from Markaby. This allows for storing and
             # redisplaying of Markaby codes
-            def smab(string, helper=nil)
+            def smab(string, helper=nil, cutat=nil)
                 builder = build_builder
                 builder.instance_eval(string)
-                return builder.to_s
+                string = builder.to_s
+                unless(cutat.nil?)
+                    index = string.index(cutat)
+                    string = string.slice(0..(index+(cutat.length - 1))) unless index.nil?
+                end
+                return string
             end
             # helpers:: Generally all that will be needed is the controller
             # you are calling from.
